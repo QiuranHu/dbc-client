ddad75195756f055e25b83b6fe30b55eafaa3b7a
Qiuran Hu <qiuranh@gmail.com>
2020-08-16 14:13:49 +0800

    modified:   package.json 	modified:   src/components/functions/create/index.tsx 	modified:   yarn.lock

--- src/components/functions/create/index.tsx
+++ src/components/functions/create/index.tsx
@@ -1,9 +1,14 @@
 import React, { FormEvent, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 import { TextField } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
 
+function Alert(props: AlertProps) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -23,7 +28,7 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export function Create(props: any) {
     const classes = useStyles();
-    const [ state, setState ] = useState({
+    const [state, setState] = useState({
         name: '',
         intro: '',
         email: '',
@@ -32,40 +37,53 @@ export function Create(props: any) {
         cardNumber: '',
         error: new Set<string>()
     });
+    const [open, setOpen] = React.useState(false);
+
+    const handleSuccess = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+      if (reason === 'clickaway') {
+        return;
+      }
+  
+      setOpen(false);
+    };
     let isValidated: boolean = false;
-    const validateEmail = (email: string):boolean => {
+    const validateEmail = (email: string): boolean => {
         const re = /\S+@\S+\.\S+/;
         return re.test(email);
     }
     const validateCardNumber = (cardNumber: string): boolean => {
         return /^\d*$/.test(cardNumber);
     }
-    if(state.name !== '' && state.email !== '' && state.cardNumber !== '') {
+    if (state.name !== '' && state.email !== '' && state.cardNumber !== '') {
         isValidated = true;
     }
-    if(state.email !== '' && !validateEmail(state.email)) {
+    if (state.email !== '' && !validateEmail(state.email)) {
         isValidated = false;
-        if(!state.error.has('email')) {
+        if (!state.error.has('email')) {
             let newError = new Set<string>(state.error);
             newError.add('email');
             setState({ ...state, error: newError });
         }
     } else {
-        if(state.error.has('email')){
+        if (state.error.has('email')) {
             let newError = new Set<string>(state.error);
             newError.delete('email');
             setState({ ...state, error: newError });
         }
     }
-    if(state.cardNumber !== '' && !validateCardNumber(state.cardNumber)) {
+    if (state.cardNumber !== '' && !validateCardNumber(state.cardNumber)) {
         isValidated = false;
-        if(!state.error.has('cardNumber')) {
+        if (!state.error.has('cardNumber')) {
             let newError = new Set<string>(state.error);
             newError.add('cardNumber');
             setState({ ...state, error: newError });
         }
     } else {
-        if(state.error.has('cardNumber')){
+        if (state.error.has('cardNumber')) {
             let newError = new Set<string>(state.error);
             newError.delete('cardNumber');
             setState({ ...state, error: newError });
@@ -74,6 +92,17 @@ export function Create(props: any) {
     const handleInputChange = (type: string, value: string): void => {
         setState({ ...state, [type]: value });
     };
+    const removeAllFields = () : void => {
+        setState({
+            name: '',
+            intro: '',
+            email: '',
+            phone: '',
+            resume: '',
+            cardNumber: '',
+            error: new Set<string>()
+        });
+    }
     const handleFormSubmit = (event: FormEvent<HTMLFormElement>): void => {
         event.preventDefault();
         fetch('/create', {
@@ -82,6 +111,15 @@ export function Create(props: any) {
                 'Content-Type': 'application/json;charset=utf-8'
             },
             body: JSON.stringify(state)
+        }).then((data) => {
+            if (data.ok) {
+                data.json().then(json => {
+                    if (json.result.ok === 1) {
+                        removeAllFields();
+                        handleSuccess();
+                    }
+                })
+            }
         })
     };
     return (
@@ -96,7 +134,7 @@ export function Create(props: any) {
                         className={classes.input}
                         variant="outlined"
                         value={state.name}
-                        required 
+                        required
                         onChange={(event: React.ChangeEvent<HTMLInputElement>) => (handleInputChange('name', event.target.value))}
                     />
                 </Grid>
@@ -107,6 +145,7 @@ export function Create(props: any) {
                         id="standard-basic"
                         label="Intro"
                         className={classes.input}
+                        value={state.intro}
                         variant="outlined"
                         onChange={(event: React.ChangeEvent<HTMLInputElement>) => (handleInputChange('intro', event.target.value))}
                     />
@@ -120,6 +159,7 @@ export function Create(props: any) {
                         label="Email"
                         className={classes.input}
                         variant="outlined"
+                        value={state.email}
                         required
                         onChange={(event: React.ChangeEvent<HTMLInputElement>) => (handleInputChange('email', event.target.value))}
                     />
@@ -132,6 +172,7 @@ export function Create(props: any) {
                         label="Phone"
                         className={classes.input}
                         variant="outlined"
+                        value={state.phone}
                         onChange={(event: React.ChangeEvent<HTMLInputElement>) => (handleInputChange('phone', event.target.value))}
                     />
                 </Grid>
@@ -142,6 +183,7 @@ export function Create(props: any) {
                         id="standard-basic"
                         label="Link to Resume"
                         className={classes.input}
+                        value={state.resume}
                         variant="outlined"
                         onChange={(event: React.ChangeEvent<HTMLInputElement>) => (handleInputChange('resume', event.target.value))}
                     />
@@ -154,6 +196,7 @@ export function Create(props: any) {
                         id="standard-basic"
                         label="Card Number"
                         className={classes.input}
+                        value={state.cardNumber}
                         variant="outlined"
                         required
                         onChange={(event: React.ChangeEvent<HTMLInputElement>) => (handleInputChange('cardNumber', event.target.value))}
@@ -165,6 +208,11 @@ export function Create(props: any) {
                         Submit
                     </Button>
                 </Grid>
+                <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                    <Alert onClose={handleClose} severity="success">
+                        Digital business card created.
+                    </Alert>
+                </Snackbar>
             </Grid>
         </form >
     )
